afe556d6f027354d253207aafac1d8af855fed09
Puja Karmakar <55836766+puja-ui@users.noreply.github.com>
2020-11-10 22:44:13 +0530

    functions

---
+++ HelloWorld.cpp
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void hello(){
+	printf("Hello World!!");
+}
+int main()
+{
+   hello(); //Calling the function
+   return 0;
+}
+//program without function
+//#include<stdio.h>
+//int main()
+//{
+//	printf("Hello World");
+//}
